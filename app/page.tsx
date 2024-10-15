24db6394f47049c6b1346f0413665838fe883489
takumi watanabe <bulk.officiall@gmail.com>
2024-10-15 18:35:18 +0900

    ３章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,49 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import mvPic from "../public/img-mv.jpg";
 
+import NewsList from "@/app/_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+import { News } from "@/app/_libs/microcms";
+
+const data: {
+	contents: News[]
+} = {
+	contents: [
+		{
+			id: 1,
+			title: "渋谷にオフィスを移転しました",
+			category: {
+				name: "更新情報",
+			},
+			publishedAt: "2023/05/19",
+			createdAt: "2023/05/19",
+		},
+		{
+			id: 2,
+			title: "当社CEOが業界リーダーTOP30に選出されました",
+			category: {
+				name: "更新情報",
+			},
+			publishedAt: "2023/05/19",
+			createdAt: "2023/05/19",
+		},
+		{
+			id: 3,
+			title: "テスト記事です。",
+			category: {
+				name: "更新情報",
+			},
+			publishedAt: "2023/04/19",
+			createdAt: "2023/04/19",
+		},
+	],
+};
+
 export default function Home() {
+	const sliceData = data.contents.slice(0, 2);
+
 	return (
+		<>
 		<section className={styles.top}>
 			<div>
 				<h1 className={styles.title}>テクノロジーで世界を変える</h1>
@@ -20,5 +61,14 @@ export default function Home() {
 				// height={1200}
 			/>
 		</section>
+		<section className={styles.news}>
+			<h2 className={styles.newsTitle}>News</h2>
+			<NewsList news={sliceData} />
+			<div className={styles.newsLink}>
+				<ButtonLink href="/news">もっと見る</ButtonLink>
+			</div>
+
+		</section>
+		</>
 	);
 }

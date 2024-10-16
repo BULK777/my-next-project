bf249f529d87ce7f9aaabc4d563e498b7efd1c0c
takumi watanabe <46353507+t-wan-7@users.noreply.github.com>
2024-10-17 01:05:58 +0900

    4章まで完了

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -3,19 +3,17 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import cx from "classnames";
 import styles from "./index.module.css";
 
 export default function Menu() {
-	const open = () => {
-		document.querySelector("nav")?.classList.add(styles.open);
-	};
-
 	const [ isOpen, setOpen ] = useState<boolean>(false);
 	const open = () => setOpen(true);
+	const close = () => setOpen(false);
 
 	return (
 		<div>
-			<nav className={styles.nav}>
+			<nav className={cx(styles.nav, isOpen && styles.open)}>
 				<ul className={styles.items}>
 				<li className={styles.item}>
 						<Link href="/news">ニュース</Link>
@@ -27,6 +25,15 @@ export default function Menu() {
 						<Link href="/contact">お問い合わせ</Link>
 					</li>
 				</ul>
+				<button className={cx(styles.button, styles.close)} onClick={close}>
+					<Image
+						src="/close.svg"
+						alt="閉じる"
+						width={24}
+						height={24}
+						priority
+					/>
+				</button>
 			</nav>
 			<button className={styles.button} onClick={open}>
 				<Image
@@ -34,6 +41,7 @@ export default function Menu() {
 					alt="メニュー"
 					width={24}
 					height={24}
+					priority
 				/>
 			</button>
 		</div>

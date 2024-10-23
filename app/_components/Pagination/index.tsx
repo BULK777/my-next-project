f780bf0af71985be7193fe47a8e329e1e2cec2d0
takumi watanabe <bulk.officiall@gmail.com>
2024-10-23 23:14:56 +0900

    ページネーション修正

--- app/_components/Pagination/index.tsx
+++ app/_components/Pagination/index.tsx
@@ -1,6 +1,6 @@
-import { NEWS_LIST_LIMIT } from "@/app/_constants";
-import Link from "next/link";
-import styles from "./index.module.css";
+import { NEWS_LIST_LIMIT } from '@/app/_constants';
+import Link from 'next/link';
+import styles from './index.module.css';
 
 type Props = {
 	totalCount: number;
@@ -11,27 +11,27 @@ type Props = {
 export default function Pagination({
 	totalCount,
 	current = 1,
-	basePath = "/news",
-}: Props) {
+	basePath = '/news',
+	}: Props) {
 	const pages = Array.from(
 		{ length: Math.ceil(totalCount / NEWS_LIST_LIMIT) },
 		(_, i) => i + 1
 	);
 
-	return(
+	return (
 		<nav>
 			<ul className={styles.container}>
-				{pages.map((p) => (
-					<li className={styles.list} key={p}>
-						{current !== p ? (
-							<Link href={`${basePath}/p/$¡{p}`} className={styles.item}>
-								{p}
-							</Link>
-						) : (
-							<span className={`${styles.item} ${styles.current}`}>{p}</span>
-						)}
-					</li>
-				))}
+			{pages.map((p) => (
+				<li className={styles.list} key={p}>
+					{current !== p ? (
+					<Link href={`${basePath}/p/${p}`} className={styles.item}>
+						{p}
+					</Link>
+					) : (
+					<span className={`${styles.item} ${styles.current}`}>{p}</span>
+					)}
+				</li>
+			))}
 			</ul>
 		</nav>
 	);
